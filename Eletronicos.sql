83dd34e15e194c1cbc528b41318987ee19c45857
Leinerhc <leinerdavidhc20@gmail.com>
2023-04-10 22:51:15 -0500

    primer procedure

--- Eletronicos.sql
+++ Eletronicos.sql
@@ -124,6 +124,31 @@ LOCK TABLES `tipo_electronico` WRITE;
 INSERT INTO `tipo_electronico` VALUES (1,'Telefonos','Pantalla táctil, cámara, conexión a internet, GPS'),(2,'Televisores','Pantalla LED, conexión a internet, aplicaciones'),(3,'Altavoz','Conexión inalámbrica, asistente de voz, calidad de sonido'),(4,'Cámara digital','	Sensor de imagen, lente, pantalla LCD, zoom'),(5,'Computadora','Pantalla LCD, teclado, disco duro, memoria RAM');
 /*!40000 ALTER TABLE `tipo_electronico` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'eletronicos'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `Descripcion&Nombre` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Descripcion&Nombre`()
+BEGIN
+SELECT aparato_electronico.DESCRIPCION,tipo_electronico.NOMBRE FROM aparato_electronico
+join tipo_electronico
+on tipo_electronico.CODIGO_T_E=aparato_electronico.TIPO_ELECTRONICO_CODIGO_T_E;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -134,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-10 22:26:29
+-- Dump completed on 2023-04-10 22:50:37
